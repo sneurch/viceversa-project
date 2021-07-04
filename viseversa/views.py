04222a553b6bb0db91a3930b98688e586be98ffc
глеб пугач <schivaser@gmail.com>
2021-07-04 17:56:50 +0300

    First commit

--- viseversa/views.py
+++ viseversa/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    user_text = request.GET['perdь']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {"usertext": user_text, 'reversedtext': reversed_text})
